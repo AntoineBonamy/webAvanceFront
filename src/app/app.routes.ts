1662850216f29a801987d62d76ba81696ed1da6d
AntoineBonamy <bonamy.antoine@hotmail.fr>
2024-03-05 09:53:25 +0100

    Router mis en place, ainsi que la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './01.pages/home-page/home-page.component';
+import { BasketPageComponent } from './01.pages/basket-page/basket-page.component';
+import { CheckoutPageComponent } from './01.pages/checkout-page/checkout-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "", component: HomePageComponent
+},
+{
+    path: "basket", component: BasketPageComponent
+},
+{
+    path: "checkout", component: CheckoutPageComponent
+}
+];
